6aeac0230563e5c9b88dae1c81e2fe52768c1af1
Emma Jauregui <emmaj819@gmail.com>
2023-11-23 16:48:46 -0800

    combined branches

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,16 +1,9 @@
-<!-- page.svelte -->
-
-<script>
-    import Calendar from "../components/calendar.svelte";
-</script>
-
-<h1>Welcome to MeetUp</h1>
+<h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
 <body>
-    <p1>Select when you are available</p1>
-
-    <!-- calendar component -->
-    <Calendar/>
-
+    <form method="post" action="connect.php">
+        Username: <input type = "text" name = "username"> <br>
+        Password: <input type = "password" name = "password"> <br>
+        <input type = "submit" value = "Submit">
+    </form> 
 </body>
