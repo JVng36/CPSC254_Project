83105cda8df0f74f45c8b0ed625715fdd5278f88
Jay Vang <vangjay36@gmail.com>
2023-11-25 20:10:16 -0800

    Lots of changes to code + DB
    now using tailwindCSS
    added register page, and can now detect what user is logged in
    will need some reconfiguration on visuals

---
+++ my-app/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let data;
+</script>
+
+<h1 class="text-3xl p-3">
+	Hello, {data.user?.username || 'Please login or register!'}!
+</h1>
+
+<h1>Welcome to MeetUp</h1>
+<body>
+    <a href="/calendar">Calendar check</a>
+	<a href="/register"> Register</a>
+</body>
---
+++ my-app/src/routes/calendar/+page.svelte
@@ -0,0 +1,17 @@
+<!-- pagetwo.svelte -->
+
+<script>
+    import Table from "../../components/table.svelte";
+</script>
+
+<h1>Welcome to Student MeetUp Scheduler</h1>
+<a href="/">Home</a>
+<body>
+    <p1>Select when you are available</p1>
+
+    <!-- calendar component removed for now-->
+
+    <!-- table component -->
+    <Table/>
+
+</body>
