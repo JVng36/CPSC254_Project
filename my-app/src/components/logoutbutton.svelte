f14f4ce10ceee225545ca2736b59c4adf2c11c5e
Jay Vang <vangjay36@gmail.com>
2023-11-27 11:07:57 -0800

    Login added
    login with error handling added

---
+++ my-app/src/components/logoutbutton.svelte
@@ -0,0 +1,39 @@
+<!-- components/LogoutButton.svelte -->
+
+<script>
+  async function handleLogout() {
+    try {
+      const response = await fetch('/logout', {
+        method: 'POST',
+      });
+
+      if (response.ok) {
+        console.log('Logout successful');
+        // Redirect or update UI as needed
+      } else {
+        console.error('Logout failed:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error during logout:', error.message);
+    }
+  }
+</script>
+
+<style>
+  /* Use :global to apply the styles globally */
+  :global(button) {
+    background-color: #007bff;
+    color: white;
+    font-weight: bold;
+    padding: 10px;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  :global(button:hover) {
+    background-color: #0056b3;
+  }
+</style>
+
+ <!-- <button on:click={handleLogout}>Logout</button>-->
--- my-app/src/routes/calendar/+page.svelte
+++ my-app/src/routes/calendar/+page.svelte
@@ -1,17 +1,51 @@
-<!-- pagetwo.svelte -->
-
 <script>
     import Table from "../../components/table.svelte";
 </script>
 
+<style>
+  /* Common styles for headings */
+  h1 {
+    font-size: 3rem;
+    padding: 1rem;
+    background-color: #cfe4fc; /* Light blue background color */
+  }
+
+  /* Styles for links */
+  a {
+    text-decoration: none;
+    color: #007bff;
+    font-weight: bold;
+    margin-right: 10px;
+  }
+
+  /* Styles for buttons */
+  .button {
+    display: inline-block;
+    padding: 10px;
+    cursor: pointer;
+    background-color: #007bff;
+    color: white;
+    font-weight: bold;
+    border: none;
+    border-radius: 5px;
+  }
+
+  .button:hover {
+    background-color: #0056b3;
+  }
+
+  /* Styles for paragraphs */
+  p1 {
+    font-size: 1.2rem;
+    margin-bottom: 10px;
+  }
+</style>
+
 <h1>Welcome to Student MeetUp Scheduler</h1>
-<a href="/">Home</a>
-<body>
-    <p1>Select when you are available</p1>
+<button class="button" on:click={() => window.location.href='/'}>Home</button>
+<p1>Select when you are available</p1>
 
-    <!-- calendar component removed for now-->
+<!-- calendar component removed for now-->
 
-    <!-- table component -->
-    <Table/>
-
-</body>
+<!-- table component -->
+<Table />
